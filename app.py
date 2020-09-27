25ef291482c87f44e31796426972b96567a5972d
Luo <forflask2020@163.com>
2020-09-27 11:06:22 +0800

    Begin index page

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import url_for
 app = Flask(__name__) #创建一个Flask对象
+name = 'Luo' 
+movies = [ 
+	{'title': 'My Neighbor Totoro', 'year': '1988'}, 
+	{'title': 'Dead Poets Society', 'year': '1989'}, 
+	{'title': 'A Perfect World', 'year': '1993'}, 
+	{'title': 'Leon', 'year': '1994'}, 
+	{'title': 'Mahjong', 'year': '1996'}, 
+	{'title': 'Swallowtail Butterfly', 'year': '1996'}, 
+	{'title': 'King of Comedy', 'year': '1999'},
+	{'title': 'Devils on the Doorstep', 'year': '1999'}, 
+	{'title': 'WALL-E', 'year': '2008'}, 
+	{'title': 'The Pork of Music', 'year': '2012'}, 
+]
 
-#注册处理函数，app.route()装饰器为这个函数绑定对应的URL
-#把'/'理解为地址，请求http://127.0.0.1:5000时就会调用hello()
 @app.route('/') 
-def hello():
-	return '<h1>Hello</h1>'
-
+def index():
+	return render_template('index.html', name=name, movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
 	return 'User: %s' % name
