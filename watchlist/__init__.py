107bce2e61f90e046c9f94b8896356eab452ddf8
Luo <forflask2020@163.com>
2020-10-02 21:32:43 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -12,8 +12,9 @@ else:
 	prefix = 'sqlite:////'
 
 app = Flask(__name__) #创建一个Flask对象
-app.config['SECRET_KEY'] = 'dev' #设置签名所需的密钥
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
+#os.getenv: 读取系统环境变量，否则为默认值
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev') 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
 
 db = SQLAlchemy(app) #在扩展类实例化前加载配置
